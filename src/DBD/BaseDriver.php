306393e399e22ee26e93157203f19d510b8e1f41
Jamie Carl <jamie@hazaarlabs.com>
2017-01-13 09:44:23 +1100

    Fixed field quotes on add constraint

--- src/DBD/BaseDriver.php
+++ src/DBD/BaseDriver.php
@@ -895,10 +895,10 @@ abstract class BaseDriver implements Driver_Interface {
         if(!array_key_exists('delete_rule', $info))
             $info['delete_rule'] = 'NO ACTION';
 
-        $sql = "ALTER TABLE " . $this->field($info['table']) . " ADD CONSTRAINT $name $info[type] ($info[column])";
+        $sql = "ALTER TABLE " . $this->field($info['table']) . " ADD CONSTRAINT " . $this->field($name) . " $info[type] (" . $this->field($info['column']) . ")";
 
         if (array_key_exists('references', $info))
-            $sql .= " REFERENCES {$info['references']['table']} ({$info['references']['column']}) ON UPDATE $info[update_rule] ON DELETE $info[delete_rule]";
+            $sql .= " REFERENCES " . $this->field($info['references']['table']) . " (" . $this->field($info['references']['column']) . ") ON UPDATE $info[update_rule] ON DELETE $info[delete_rule]";
 
         $sql .= ';';
 
