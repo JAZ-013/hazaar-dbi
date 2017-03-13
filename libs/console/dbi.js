c151da418cd504035b81b4c4420e5bf2fe73af4a
Jamie Carl <jamie@hazaarlabs.com>
2017-03-13 19:53:06 +1100

    Ported the app console stuff over

---
+++ libs/console/dbi.js
@@ -0,0 +1,36 @@
+﻿function showLog(obj, result) {
+    var panel = obj.parent().removeClass('panel-default');
+    if (result.ok)
+        panel.addClass('panel-success');
+    else
+        panel.addClass('panel-danger');
+    for (x in result.log) {
+        var thetime = new Date(result.log[x].time * 1000);
+        obj.append($('<div>').append($('<div class="log-time">').html(thetime.toLocaleString()), $('<div class="log-msg">').html(result.log[x].msg)));
+    }
+}
+$(document).ready(function () {
+    $('#frmSnapshot').submit(function () {
+        var data = $(this).serializeArray();
+        $('#snapshotlog').empty().parent().addClass('panel-default').removeClass('panel-success').removeClass('panel-danger');
+        $.post(hazaar.url('dbi', 'snapshot'), data).done(function (result) {
+            showLog($('#snapshotlog'), result);
+        });
+        return false;
+    });
+    $('#frmMigrate').submit(function () {
+        var data = $(this).serializeArray();
+        $('#migratelog').empty().parent().addClass('panel-default').removeClass('panel-success').removeClass('panel-danger');
+        $.post(hazaar.url('dbi', 'migrate'), data).done(function (result) {
+            showLog($('#migratelog'), result);
+        });
+        return false;
+    });
+    $('#frmSync').submit(function () {
+        $('#synclog').empty().parent().addClass('panel-default').removeClass('panel-success').removeClass('panel-danger');
+        $.post(hazaar.url('dbi', 'syncdata')).done(function (result) {
+            showLog($('#synclog'), result);
+        });
+        return false;
+    });
+});
--- src/Console.php
+++ src/Console.php
@@ -12,13 +12,75 @@ class Console extends \Hazaar\Console\Module {
 
         $this->addMenuItem('dbi', 'Data Sync', 'sync');
 
+        $this->view->requires('dbi.js');
+
     }
 
     public function schema(){
 
+        $db = new \Hazaar\DBI\Adapter();
+
+        $this->view('schema');
+
+        $current = $db->getSchemaVersion();
+
+        $versions = array('latest' => 'Latest Version') + $db->getSchemaVersions();
+
+        $this->view->current_version = $current . ' - ' . ake($versions, $current, 'missing');
+
+        $this->view->versions = $versions;
+
+        $this->view->latest = $db->isSchemaLatest();
+
     }
 
-    public function sync(){
+    public function snapshot($request){
+
+        if(!$request->isPOST())
+            return false;
+
+        $db = new \Hazaar\DBI\Adapter();
+
+        $result = $db->snapshot($request->get('comment'), boolify($request->get('testmode', false)));
+
+        return array('ok' => $result, 'log' => $db->getMigrationLog());
+
+    }
+
+    public function migrate($request){
+
+        if(!$request->isPOST())
+            return false;
+
+        $version = $this->request->get('version', 'latest');
+
+        if($version == 'latest')
+            $version = null;
+
+        $db = new \Hazaar\DBI\Adapter();
+
+        $result = $db->migrate($version, boolify($request->get('testmode', false)));
+
+        return array('ok' => $result, 'log' => $db->getMigrationLog());
+
+    }
+
+    public function sync($request){
+
+        $this->view('sync');
+
+    }
+
+    public function syncdata($request){
+
+        if(!$request->isPOST())
+            return false;
+
+        $db = new \Hazaar\DBI\Adapter();
+
+        $result = $db->syncSchemaData();
+
+        return array('ok' => $result, 'log' => $db->getMigrationLog());
 
     }
 
